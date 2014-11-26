d295ace7cbe51a81e6f95637b913ee602c10fe39
Michael Tsai <huanlin.tsai@gmail.com>
2014-11-27 00:35:41 +0800

    第 5 章增加 Autofac 與 Web API 2 整合的範例。

---
+++ Examples/ch05/Ex02.DependencyResolver.AutofacDemo/App_Start/WebApiConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Ch05.Common.Services;
+
+namespace Ex02.DependencyResolver.AutofacDemo
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            var builder = new ContainerBuilder();
+            builder.RegisterApiControllers(typeof(WebApiConfig).Assembly);
+            builder.RegisterType<CustomerService>().As<ICustomerService>(); // RegisterType<實作類別>().As<抽象型別>()
+
+            var container = builder.Build();
+            var resolver = new AutofacWebApiDependencyResolver(container);
+            config.DependencyResolver = resolver; 
+        }
+    }
+}
---
+++ Examples/ch05/Ex02.DependencyResolver.AutofacDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ex02.DependencyResolver.AutofacDemo.WebApiApplication" Language="C#" %>
