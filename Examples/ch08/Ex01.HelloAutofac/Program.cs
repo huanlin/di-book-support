e94f28d88a26c4e006f8164fed509d6e8b7378c0
Michael Tsai <huanlin.tsai@gmail.com>
2015-06-16 01:29:51 +0800

    Added sample code of Chapter 8.

---
+++ Examples/ch08/Ex01.HelloAutofac/Program.cs
@@ -0,0 +1,29 @@
+﻿        using Autofac;
+        
+        namespace Ex01.HelloAutofac
+        {
+            class Program
+            {
+                private static IContainer _container;
+        
+                static void Main(string[] args)
+                {
+                    var builder = new ContainerBuilder();
+                    builder.RegisterType<SayHelloInChinese>().As<ISayHello>();
+        
+                    _container = builder.Build();
+        
+                    SayHello();
+                }
+        
+                static void SayHello()
+                {
+                    using (var scope = _container.BeginLifetimeScope())
+                    {
+                        ISayHello hello = _container.Resolve<ISayHello>();
+                        hello.Run();
+                    }                
+                }
+            }
+        }
+        
---
+++ Examples/ch08/Ex01.HelloAutofac/SayHelloInChinese.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01.HelloAutofac
+{
+    class SayHelloInChinese : ISayHello, IDisposable
+    {
+        public void Run()
+        {
+            Console.WriteLine("哈囉, Autofac!");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("SayHelloInChinese.Dispose()");
+        }
+    }
+}
